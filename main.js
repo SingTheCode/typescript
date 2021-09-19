2100fecf7d460fe801c4c02987a8009114e51b66
spiderq2697 <spiderq10@gmail.com>
2021-09-19 11:12:26 +0900

    타입스크립트란?

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello World!@@@@");
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
---
+++ main.ts
@@ -0,0 +1,4 @@
+console.log("Hello World!@@@@");
+class Car {
+  num: number;
+}
