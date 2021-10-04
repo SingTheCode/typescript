0d136f098cca261e3898f5ec10498dcc1fab05a6
spiderq2697 <spiderq10@gmail.com>
2021-10-04 14:03:27 +0900

    union type 사용

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,27 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
+
+type Motion = "add" | "substract" | "multiply" | "divide" | "remainder";
+
+function calculate(motion: Motion, ...answer: Array<number>): number {
+  switch (motion) {
+    case "add":
+      return answer[0] + answer[1];
+    case "substract":
+      return answer[0] - answer[1];
+    case "multiply":
+      return answer[0] * answer[1];
+    case "divide":
+      return answer[0] / answer[1];
+    case "remainder":
+      return answer[0] % answer[1];
+    default:
+      throw new Error("unknown error");
+  }
+}
