7bb1ac75d278d5f943e8a4424613eae138f119fa
spiderq2697 <spiderq10@gmail.com>
2021-10-04 14:12:59 +0900

    Position type alias 생성

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,43 @@
 /**
  * Let's make a game 🕹
  */
+
+type Command = "up" | "down" | "left" | "right";
+type Position = {
+  x: number;
+  y: number;
+};
+
+let position: Position = {
+  x: 0,
+  y: 0,
+};
+
+function move(command: Command) {
+  switch (command) {
+    case "up":
+      position.y += 1;
+      break;
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      throw new Error("command is wrong");
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
